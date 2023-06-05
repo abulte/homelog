d5fd413bd893a1394413533b2b84392cc4694b34
Alexandre Bulté <alexandre@bulte.net>
2023-06-05 13:49:34 +0200

    Such secure middleware

---
+++ README.md
@@ -0,0 +1,1 @@
+# homelog
--- homelog/app.py
+++ homelog/app.py
@@ -1,7 +1,16 @@
+import os
+
 from flask import Flask, request
 
 
 app = Flask(__name__)
+
+
+@app.before_request
+def check_api_key():
+    api_key = request.headers.get("x-api-key")
+    if api_key != os.getenv("HOMELOG_API_KEY"):
+        return "Unauthorized", 401
 
 
 @app.route("/api/temperature", methods=["POST"])
